afc98f749e4dcfc0f9eb0975ce5d751ea89b9e37
Erik Nava <erik.nava@gmail.com>
2019-12-19 16:29:05 -0600

    Updated to use the form_for helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,15 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.update(params[:id], user_params)
+    redirect_to edit_user_path
+  end
+
   private
     def user_params
       params.require(:user).permit(:username, :email, :password,
